e82e7c8bc8fac285a279477b943859a6e5a7c07c
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-10-03 13:46:13 +0000

    foreignkeys no funcionan

--- src/api/models.py
+++ src/api/models.py
@@ -12,12 +12,13 @@ class Roomie(db.Model):
     phone_number = db.Column(db.Integer, unique=True, nullable=False)
     avatar = db.Column(db.String(200), nullable=False)
     paypal_id = db.Column(db.String(12), nullable=False)
-    #expenses = db.relationship('Expenses', secondary='roomie_debts', backref='roomie', lazy=True)
+
+    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+
+    expenses = db.relationship('Expenses', secondary='roomie_debts', backref='roomie', lazy=True)
     tasks = db.relationship('Task', backref='roomie')
-    #blogs = db.relationship('Blog', backref='roomie')
-    #notification = db.relationship('Notifications', backref='roomie')
-    #home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
-    #task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
+    blogs = db.relationship('Blog', backref='roomie')
+    notification = db.relationship('Notifications', backref='roomie')
 
     def __repr__(self):
         return '<Roomie %r>' % self.id
@@ -26,6 +27,7 @@ class Roomie(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
             "first_name": self.first_name,
             "last_name": self.last_name,
             "phone_number": self.phone_number,
@@ -36,10 +38,11 @@ class Roomie(db.Model):
 class Home(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    #expenses = db.relationship('Expenses', backref='home')
-    #shopping_lists = db.relationship('ShoppingList', backref='home')
-    #files = db.relationship('File', backref='home')
-    #roomies = db.relationship('Roomie', backref='home')
+    
+    expenses = db.relationship('Expenses', backref='home', lazy=True)
+    shopping_lists = db.relationship('ShoppingList', backref='home', lazy=True)
+    files = db.relationship('File', backref='home', lazy=True)
+    roomies = db.relationship('Roomie', backref='home', lazy=True)
 
     def __repr__(self):
         return '<Home %r>' % self.id
@@ -53,10 +56,12 @@ class Home(db.Model):
 class Expenses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    #home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
-    #roomie_debts = db.relationship('RoomieDebts', backref='expense', lazy=True, overlaps="roomie")
-    #shopping_items = db.relationship('ShoppingItems', backref='expenses', lazy=True)
-    #files = db.relationship('File', backref='expenses', lazy=True)
+    
+    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    
+    roomie_debts = db.relationship('RoomieDebts', backref='expense', lazy=True, overlaps="roomie")
+    shopping_items = db.relationship('ShoppingItems', backref='expenses', lazy=True)
+    files = db.relationship('File', backref='expenses', lazy=True)
     
     def __repr__(self):
         return '<Expenses %r>' % self.id
@@ -65,11 +70,11 @@ class Expenses(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            #"home": self.home_id
+            "home": self.home_id
         }
 
 class RoomieDebts(db.Model):
-    #roomie_id = db.Column(db.Integer, db.ForeignKey('roomie.id'), primary_key=True)
+    roomie_id = db.Column(db.Integer, db.ForeignKey('roomie.id'), primary_key=True)
     expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id'), primary_key=True)
     amount = db.Column(db.Integer, nullable=False)
     roomie_debtor = db.Column(db.String(20), nullable=False)
@@ -82,7 +87,7 @@ class RoomieDebts(db.Model):
     def serialize(self):
         return {
             "expenseID": self.expense_id,
-            #"roomieID": self.roomie_id,
+            "roomieID": self.roomie_id,
             "amount": self.amount,
             "roomieDebtor": self.roomie_debtor,
             "status": self.status,
@@ -91,26 +96,27 @@ class RoomieDebts(db.Model):
 
 class ShoppingList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    #shopping_items = db.relationship('ShoppingItems', backref='shopping_list', lazy=True)
-
+    
+    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    
     def __repr__(self):
         return '<ShoppingList %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            #"home": self.home_id,
+            "home_id": self.home_id,
             "name": self.name
         }
 
 class ShoppingItems(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #shopping_list_id = db.Column(db.Integer, db.ForeignKey('shopping_list.id'), nullable=False)
-    #expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     checked = db.Column(db.Boolean, nullable=False)
+
+    shopping_list_id = db.Column(db.Integer, db.ForeignKey('shopping_list.id'), nullable=False)
+    expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id'), nullable=False)
 
     def __repr__(self):
         return '<ShoppingItems %r>' % self.id
@@ -118,20 +124,19 @@ class ShoppingItems(db.Model):
     def serialize(self):
         return {
             "itemID": self.id,
-            #"listID": self.shopping_list_id,
+            "listID": self.shopping_list_id,
             "name": self.name,
             "checked": self.checked
         }
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    roomie_id = db.Column(db.Integer, db.ForeignKey('roomie.id'),
-        nullable=False)
-    #roomie = db.relationship('Roomie', backref='task')
     name = db.Column(db.String(50), nullable=False)
     date_added = db.Column(db.Date, nullable=False)
     date_done = db.Column(db.Date, nullable=False)
     done = db.Column(db.Boolean, nullable=False)
+
+    roomie_id = db.Column(db.Integer, db.ForeignKey('roomie.id'), nullable=False)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -148,11 +153,12 @@ class Task(db.Model):
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
-    #expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     date = db.Column(db.Date, nullable=False)
     url = db.Column(db.String(200), nullable=False)
+
+    home_id = db.Column(db.Integer, db.ForeignKey('home.id'), nullable=False)
+    expense_id = db.Column(db.Integer, db.ForeignKey('expenses.id'), nullable=False)
 
     def __repr__(self):
         return '<File %r>' % self.id
@@ -160,8 +166,8 @@ class File(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            #"home": self.home_id,
-            #"expense": self.expense_id,
+            "home": self.home_id,
+            "expense": self.expense_id,
             "name": self.name,
             "date": str(self.date),
             "url": self.url
@@ -169,11 +175,12 @@ class File(db.Model):
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #roomie_id = db.Column(db.Integer, db.ForeignKey('roomie.id'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     date = db.Column(db.Date, nullable=False)
     status = db.Column(db.String(20), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
+
+    roomie_id = db.Column(db.Integer, db.ForeignKey('roomie.id'), nullable=False)
 
     def __repr__(self):
         return '<Blog %r>' % self.id
@@ -181,7 +188,7 @@ class Blog(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            #"roomie": self.roomie_id,
+            "roomie": self.roomie_id,
             "name": self.name,
             "status": self.status,
             "amount": self.amount,
@@ -190,8 +197,9 @@ class Blog(db.Model):
 
 class Notifications(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #phone_number = db.Column(db.Integer, db.ForeignKey('roomie.phone_number'), nullable=False)
     name = db.Column(db.String(20), nullable=False)
+
+    phone_number = db.Column(db.Integer, db.ForeignKey('roomie.phone_number'), nullable=False)
 
     def __repr__(self):
         return '<Notifications %r>' % self.id
@@ -199,6 +207,7 @@ class Notifications(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            #"phone_number": self.phone_number,
+            "phone_number": self.phone_number,
             "name": self.name
         }
+
