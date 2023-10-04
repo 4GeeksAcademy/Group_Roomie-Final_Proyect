551cb806673b3ccddc09d1379a54878902748f51
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-10-04 14:18:48 +0000

    añado rutas api para tasks

--- src/api/routes.py
+++ src/api/routes.py
@@ -215,3 +215,36 @@ def get_one_task(task_id):
         return jsonify({'error': 'Esta tarea no existe'}), 404
     return jsonify(chosen_task.serialize()), 200
 
+@api.route('/task/<int:roomie_id>', methods=['GET'])
+def get_tasks_by_roomie_id(roomie_id):
+    tasks = Task.query.filter_by(roomie_id=roomie_id).all()
+    task_list = [task.serialize() for task in tasks]
+    return jsonify(task_list), 200
+
+@api.route('/task/create', methods=['POST'])
+def create_task():
+    request_data = request.get_json()
+    roomie_id = request_data.get('roomie_id')
+    if roomie_id is None:
+        return jsonify({'error': 'Es necesario asignar un roomie'}), 400
+    new_task = Task(
+        name=request_data.get('name'),
+        date_added=request_data.get('date_added'),
+        date_done=request_data.get('date_done'),
+        done=request_data.get('done'),
+        roomie_id=roomie_id
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Nueva tarea añadida correctamente' }), 200
+
+@api.route('/task/delete/<int:roomie_id>/<int:task_id>', methods=['DELETE'])
+def delete_task(roomie_id, task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify({'error': 'La tarea no existe'}), 400
+    if task.roomie_id != roomie_id:
+        return jsonify({'error': 'Esta tarea no pertenece a este roomie'}), 400
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Tarea eliminada correctamente'}), 200
